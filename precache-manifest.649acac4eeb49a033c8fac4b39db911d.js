bbd65338759893a40dbc80b843a92735b19f4acd
KremeshevD <56875179+KremeshevD@users.noreply.github.com>
2020-05-18 12:23:48 +0300

    Updates

---
+++ precache-manifest.649acac4eeb49a033c8fac4b39db911d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "dc64a05b96720ddd17ef1cfc5a536a79",
+    "url": "/to_do_list/index.html"
+  },
+  {
+    "revision": "e0afae0d59031eeae7cc",
+    "url": "/to_do_list/static/css/main.9aaa8ea8.chunk.css"
+  },
+  {
+    "revision": "5dcfea5391eee673d4c2",
+    "url": "/to_do_list/static/js/2.8087352e.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/to_do_list/static/js/2.8087352e.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e0afae0d59031eeae7cc",
+    "url": "/to_do_list/static/js/main.62ab4780.chunk.js"
+  },
+  {
+    "revision": "644d66d14609d9890072",
+    "url": "/to_do_list/static/js/runtime-main.2c9a17ff.js"
+  }
+]);
